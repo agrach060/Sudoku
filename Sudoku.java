0ee163e85c1ca50c1ff017e87b9d6e837356b01e
agrach060 <128651743+agrach060@users.noreply.github.com>
2023-04-26 00:02:39 -0700

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -1,7 +1,6 @@
 // Anna Gracheva
 // CS 143
-// HW Core Topics: Sudoku #1(Board Setup)
-//
+
 // This program will import a Sudoku from a file and print the board to the screen
 
 import java.util.*;
@@ -26,23 +25,3 @@ public class Sudoku{
 
    }
 }
-
-/*     ----jGRASP exec: java Sudoku
- Please enter the file name: 
- data1.sdk
-  
-  2  _  _   1  _  5   _  _  3 
-  _  5  4   _  _  _   7  1  _ 
-  _  1  _   2  _  3   _  8  _ 
- 
-  6  _  2   8  _  7   3  _  4 
-  _  _  _   _  _  _   _  _  _ 
-  1  _  5   3  _  9   8  _  6 
- 
-  _  2  _   7  _  1   _  6  _ 
-  _  8  1   _  _  _   2  4  _ 
-  7  _  _   4  _  2   _  _  1 
- 
- 
-  ----jGRASP: operation complete.
- */
